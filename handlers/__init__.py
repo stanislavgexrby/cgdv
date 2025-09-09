d37047a938c66b7bcad0555712d17a541c50c91d
stanislav <stasereser@gmail.com>
2025-09-09 19:40:20 +0300

    restructured profile

--- handlers/__init__.py
+++ handlers/__init__.py
@@ -1,8 +1,9 @@
 from aiogram import Dispatcher
-from . import basic, profile, search, likes
+from . import basic, profile, search, likes, profile_editing
 
 def register_handlers(dp: Dispatcher):
     dp.include_router(profile.router)
+    dp.include_router(profile_editing.router)
     dp.include_router(search.router)
     dp.include_router(likes.router)
     dp.include_router(basic.router)
